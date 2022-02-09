d2f948a6e624edff707f6dd2fbc3eaed90711470
omkar2120 <naradomkar212000@gmail.com>
2022-02-09 11:34:14 +0530

    initial commit

--- frontend/src/global/component/Notfound.js
+++ frontend/src/global/component/Notfound.js
@@ -9,6 +9,7 @@ const Notfound = () => {
 		<div className="notfound">
 			<div className="notfound-404">
 				<h1>Oops!</h1>    
+                
 				<h2>404 - The Page can't be found</h2>
 			</div>
 			
